bf29b52f9d8fd2c5ad471251e3b8328ea41f27ed
EBarmentlo <93001558+EBarmentlo@users.noreply.github.com>
2022-06-04 11:41:54 +0200

    added github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather application</div>;
+  return (
+    <div className="App">
+      Weather application
+      <footer>
+        This project is created by Elisabeth Barmentlo and it is {""}
+        <a
+          href="https://github.com/EBarmentlo/react-weather-app"
+          target="-blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
