5da78a1f7b597b11ae9004579aaba2cd5846ce57
EBarmentlo <93001558+EBarmentlo@users.noreply.github.com>
2022-06-04 12:33:28 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather application
-      <footer>
-        This project is created by Elisabeth Barmentlo and it is {""}
-        <a
-          href="https://github.com/EBarmentlo/react-weather-app"
-          target="-blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather application
+        <footer>
+          This project is created by Elisabeth Barmentlo and it is{" "}
+          <a
+            href="https://github.com/EBarmentlo/react-weather-app"
+            target="-blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
